ec6bfb27812bfabc878abca43b956a9983ee40d5
Ryan Woosley <s592m3@yahoo.com>
2021-02-06 15:47:39 -0600

    HW #1 Updated with notes

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -8,14 +8,17 @@
 
 int main() 
 {
+	// Creates variable num > Gets a number from the user > assigns it to num
 	int num; 
 	cout<<"Enter a number: ";
 	cin >> num;
 
+	// Creates variable result > Result equals num1(user input) * num2(5) > Displays the result 
 	int result;
 	result = multiply_numbers(num);
 	cout<< "The total is: " << result << "\n";
 
+	// Creates num1 and assigns it to 4 > Takes the num1(4) * num2(5) and displays the result of num1 * num2 
 	int num1 = 4;
 	result = multiply_numbers(num1);
 	cout<< "The total is: " << result << "\n";
